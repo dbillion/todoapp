f1454739bed5f5a24d25d06f24aca3676291fa38
oludayoadeoye <oludayoadeoye99@gmail.com>
2023-10-12 09:10:19 +0300

    chapter 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'todoapp';
+export class AppComponent implements OnDestroy{
+  ngOnDestroy(): void {
+    
+  }
+  title = 'dayos app';
+  myText="world"
 }
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,42 @@
+
+import { AfterViewInit, Component, ViewChild, ChangeDetectorRef } from '@angular/core';
+import { ProductDetailComponent } from '../product-detail/product-detail.component';
+
+import { Product } from '../product';
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.scss']
+})
+export class ProductListComponent implements AfterViewInit{
+
+ selectedProduct : Product | undefined;
+ products: Product[] = [
+  {
+  name: 'Webcam',
+  price: 100
+  },
+  {
+    name:
+    'Microphone',
+    price: 200
+    },
+    {
+    name: 'Wireless keyboard',
+    price: 85
+    }
+    ];
+ @ViewChild(ProductDetailComponent) productDetail: ProductDetailComponent | undefined;
+ ngAfterViewInit(): void {
+  if (this.productDetail) {
+  console.log(this.productDetail.product);
+  }
+  }
+  onBuy() {
+  window.alert(`You just bought ${this.selectedProduct?.name}!`);
+  }
+    trackByProducts(index: number, name: string): string {
+      return name;
+      }
+
+}
